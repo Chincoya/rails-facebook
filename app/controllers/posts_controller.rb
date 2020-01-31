04f4bd8a48fddc2728dc1017c9ec0187d1c1f8d6
Erik Nava <erik.nava@gmail.com>
2020-01-31 15:08:18 -0600

    applied suggested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @posts = Post.all.order('created_at DESC')
+    @post = Post.new
   end
 
   def new
