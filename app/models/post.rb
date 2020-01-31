d152b207e5f165472c27f67cb8c7d66e67682def
Erik Nava <erik.nava@gmail.com>
2020-01-31 15:36:12 -0600

    applied changes to likes partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable
   has_many :comments
+
+  def like_count
+    likes.count
+  end
+
+  def liked_by(current_user)
+    current_user.likes.find_by(likeable_id: id)
+  end
 end
