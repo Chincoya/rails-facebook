2ae077e6f1ae7ca1803ea7390ffa7f5541b07d3e
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-02-10 13:59:34 -0600

    Add two row implementation of frienships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,22 +3,38 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
   def create
+    @user = current_user
     friendship_id = friend_params[:user_id]
-    current_user.friendships.create(friend_id: friendship_id)
+    new_friend = Friendship.new(user_id: friendship_id, friend_id: @user.id)
+    if new_friend.save
+      @user.friendships.create(friend_id: friendship_id, confirm: true)
+      flash[:sucess] = 'Friend Added'
+    else
+      flash[:danger] = 'Error adding friend'
+    end
     redirect_to request.referer
   end
 
   def destroy
     friendship_id = params[:id]
+    friendship = Friendship.find(friendship_id)
+    inverse = Friendship.where('user_id = ? and friend_id = ?', friendship.friend_id, friendship.user_id)
     Friendship.delete(friendship_id)
+    Friendship.delete(inverse[0].id)
     redirect_to request.referer
   end
 
   def update
     friendship_id = params[:id]
     friendship = Friendship.find(friendship_id)
-    friendship.confirm = true if friendship.friend_id == current_user.id
-    friendship.save
+    inverse = Friendship.where('user_id = ? and friend_id = ?', friendship.friend_id, friendship.user_id)
+    if friendship.user_id == current_user.id
+      friendship.confirm = true
+      inverse[0].confirm = true
+      friendship.save(validate: false)
+      inverse[0].save(valdiate: false)
+      flash[:sucess] = 'Frienship Confirmed'
+    end
     redirect_to request.referer
   end
 
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class Friendship < ApplicationRecord
+  validate :duplicate
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   scope :confirmed, -> { where(confirm: true) }
   scope :unconfirmed, -> { where(confirm: nil) }
 
   def sent_name
-    User.find(user_id).name
+    User.find(friend_id).name
+  end
+
+  private
+
+  def duplicate
+    return unless User.find(user_id).friendships.find_by(friend_id: friend_id)
+
+    errors.add(:base, 'No duplicate values')
   end
 end
