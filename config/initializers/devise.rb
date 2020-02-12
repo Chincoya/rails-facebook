522dd9d69fa2a694837de6d29ed74aeb4a9aee0b
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-02-12 17:22:33 -0600

    Hotfix: API keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :facebook, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, callback_url: "https://immense-shelf-90273.herokuapp.com/users/auth/facebook"
   User.omniauth_providers.each do |provider_name|
     if provider_name == :developer
       config.omniauth :developer
